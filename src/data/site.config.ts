35158ce15d70d191aa13eb806b4f242474aabef2
asyrinarisha <asyraf.senpai@yahoo.com.my>
2026-06-09 11:28:00 +0800

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,9 +10,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	site: 'https://gudanginfo.my/', // Write here your website url
+	author: 'Asyraf', // Site author
+	title: 'Laman Informasi Anda', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
